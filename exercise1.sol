4f0247de3ab30046a2aefdfe06902b89f35ee191
Dhruva-S-04 <134116727+Dhruva-S-04@users.noreply.github.com>
2025-03-19 10:35:35 +0530

    Delete exercise1.sol

--- exercise1.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-contract Exercise1 {
-    uint public wallet = 500;              // Step 4: Initialize wallet to 500
-    bool public spend = false;             // Step 5: Set spend to false
-    string public notifySpend = "You have spent money"; // Step 6: Assign string literal
-}
